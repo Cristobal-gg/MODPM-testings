a18a7b6129d5792966eb42e3b410454aea389f70
Cristobal Gonzalez <cgonzalar@sonda.com>
2024-08-22 10:27:15 -0400

    Test API and documentation
    Simple API test and documentation

--- integrationsTesting/testings/serializers.py
+++ integrationsTesting/testings/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import Book
 
+#Clase que convierte el modelo Book serializandolo en formato JSON
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['id', 'title', 'author', 'published_date']
+        fields = ['id', 'title', 'author', 'published_date'] #campos a serializar del modelo
