c3d2fb9700c7b3c4707b96ce7e08859e8a7d13ac
Cristobal Gonzalez <cgonzalar@sonda.com>
2024-08-23 11:15:28 -0400

    Connection between Back-end and Front-end
    React.js consumes Django rest framework API

--- integrationsTesting/clienteMODPM/src/components/Booklist.jsx
+++ integrationsTesting/clienteMODPM/src/components/Booklist.jsx
@@ -1,15 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {getAllBooks} from "../api/Booklist.api";
 
 
 export function Booklist() {
 
+    const [books, setBooks] = useState([]);
+
     useEffect(() => {
-        function fetchBooks() {
-            const books = getAllBooks();
-            console.log(books);
+        async function fetchBooks() {
+            const books = await getAllBooks();
+            setBooks(books.data);
         }
         fetchBooks();
     }, []);
-    return <div>Booklist</div>;
+    return <div>Booklist
+
+        {books.map(book => (
+            <div key={book.id}>
+                <h2>{book.title}</h2>
+                <p>{book.author}</p>
+            </div>
+        ))}
+    </div>;
 }
--- integrationsTesting/core/settings.py
+++ integrationsTesting/core/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [] #cors authorization
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173"] #cors authorization
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
